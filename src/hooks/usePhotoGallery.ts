6fc48d1ad5cbd50df18f98d53d89cf075b86d663
Shashank <snaik2064@gmail.com>
2020-03-01 13:40:55 +0530

    Saving & Loading photo from the filesystem complete.

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useCamera } from '@ionic/react-hooks/camera';
 import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
 import { useStorage } from '@ionic/react-hooks/storage';
@@ -17,9 +17,29 @@ export interface Photo {
   base64?: string;
 }
 
+const PHOTO_STORAGE = 'photos';
+
 export function usePhotoGallery() {
   const { getPhoto } = useCamera();
+  const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+  const { get, set } = useStorage();
   const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photosString = await get('photos');
+      const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+      for (let photo of photos) {
+        const file = await readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
+        photo.base64 = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photos);
+    };
+    loadSaved();
+  }, [get, readFile]);
 
   const takePhoto = async () => {
     const cameraPhoto = await getPhoto({
@@ -28,14 +48,41 @@ export function usePhotoGallery() {
       quality: 100
     });
     const fileName = new Date().getTime() + '.jpeg';
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: cameraPhoto.webPath
-      },
-      ...photos
-    ];
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    set(
+      PHOTO_STORAGE,
+      JSON.stringify(
+        newPhotos.map(p => {
+          // Don't save the base64 representation of the photo data,
+          // since it's already saved on the Filesystem
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+          return photoCopy;
+        })
+      )
+    );
+  };
+
+  const savePicture = async (photo: CameraPhoto, fileName: string) => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    await writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+    return getPhotoFile(photo, fileName);
+  };
+
+  const getPhotoFile = async (
+    cameraPhoto: CameraPhoto,
+    fileName: string
+  ): Promise<Photo> => {
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
   };
 
   return {
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -11,12 +11,10 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonImg,
-  IonActionSheet
+  IonImg
 } from '@ionic/react';
 import { camera, trash, close } from 'ionicons/icons';
 
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
@@ -35,7 +33,7 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.webviewPath} />
+                <IonImg src={photo.base64 ?? photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
@@ -45,12 +43,6 @@ const Tab2: React.FC = () => {
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
-        {/* <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" /> */}
       </IonContent>
     </IonPage>
   );
